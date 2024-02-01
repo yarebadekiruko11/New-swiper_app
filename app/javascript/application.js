77895bc3bc9df1ada197f1de660b9a79492f5207
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2024-02-01 15:35:20 +0000

    動作確認できず

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,17 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 // import "jquery"
-import "./script.js"
+import "./script.js";
 // import "./add_jquery"
-import jquery from "jquery"
-window.$ = jquery
+import jquery from "jquery";
+window.$ = jquery;
 
-//= require jquery
-//= require jquery_ujs
-
-$(document).ready(function () {
-  $('.jquery').on('click', function(){
-    $(this).css('color','red');
-  });
-});
